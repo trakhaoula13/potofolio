4e180c3c02e8457341bb2ce553994e792e8e1265
khaoula <trabelsikhaoula556@gmail.com>
2026-07-25 15:30:30 +0000

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+document.documentElement.setAttribute('data-bs-theme', 'light');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
