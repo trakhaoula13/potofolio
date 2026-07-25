0a5a59b890747bf83f8c5969c8f0744c92c1a965
khaoula <trabelsikhaoula556@gmail.com>
2026-07-25 21:45:17 +0000

    test4

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-document.documentElement.setAttribute('data-bs-theme', 'somber');
+document.documentElement.setAttribute('data-bs-theme', 'light'); // ✅ au lieu de 'somber'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
